a22c18017466d05a523b1c539bccf59e1f1cca5f
ankurholz <aholz@med.umich.edu>
2023-12-13 23:03:51 -0500

    Initial commit

---
+++ Chapter_2.R
@@ -0,0 +1,13 @@
+##2.1 Coding basics
+
+# <- assignment operator to create new objects. object_name <- value
+
+x <- 3*7
+
+# 'c' combines elements into a vector
+
+primes <- c(2,3,5,7,11,13)
+primes * 2
+
+# alt minus is the shortcut for the assignment operator
+
