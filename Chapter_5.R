9ba905565dc08ec4482f027d0690755f84bcd7d7
ankurholz <aholz@med.umich.edu>
2024-01-21 22:44:24 -0500

    Chapter 5 notes progress

--- Chapter_5.R
+++ Chapter_5.R
@@ -21,6 +21,8 @@ table2 <- read.csv("Chapter5_table2.csv")
 pivot_wider()
 pivot_longer()
 
+#pivot_longer(data, cols, names_to, values_to)
+
 billboard #billboard data set from tidyverse
 
 billboard_pl <- billboard %>%
@@ -28,4 +30,42 @@ billboard_pl <- billboard %>%
                names_to = "week",
                values_to = "rank")
 
-print(billboard_pl,n=20)
+billboard_pl <- billboard %>%
+  pivot_longer(!c(artist, track, date.entered),
+               names_to = "week",
+               values_to = "rank",
+               values_drop_na = TRUE) %>%
+  mutate(
+    week = parse_number(week)
+  )
+
+billboard_pl %>%
+  ggplot(aes(x = week, y = rank, group = track)) +
+  geom_line(alpha = 0.25) +
+  scale_y_reverse()
+  
+bp <- tribble(
+  ~id,  ~bp1, ~bp2,
+  "A",  100,  120,
+  "B",  140,  115,
+  "C",  120,  125
+)
+
+bp_p <- bp %>%
+  pivot_longer(cols = !c(id),
+               names_to = "measurement",
+               values_to = "value"
+               )
+#5.3.2 Many variables in column names, using names_sep
+
+#names_sep =
+#names_pattern
+
+table3 <- who2
+
+table3_p <- table3 %>%
+  pivot_longer(cols = !(country:year),
+               names_to = c("method", "sex", "age group"), 
+               names_sep = "_",
+               values_to = "cases",
+               values_drop_na = TRUE)
